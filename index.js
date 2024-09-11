fee90410548cc5cd29bc4005942e67c10db2993a
Jonas Almeida <jalmeida@mathbiol.org>
2024-09-11 06:05:23 -0400

    relative location

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 console.log(`index.js loaded\n${Date()}`);
 
-// module
-GEM = (await import('http://localhost:8000/gemini/gem.mjs')).GEM
-
 // application
 (async function(){
     console.log(`async runtime\n${Date()}`);
-    
-    
-})()
+    GEM = (await import(`${location.href}gem.mjs`)).GEM
+})();
