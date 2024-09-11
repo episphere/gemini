3f64b0fd2c3983ee2d861bfe5dc1f98879325869
Jonas Almeida <jalmeida@mathbiol.org>
2024-09-11 08:35:35 -0400

    ui action

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 console.log(`index.js loaded\n${Date()}`);
 
+// UI action
+setTimeout(function() {
+    (document.getElementById('provideKey')).onclick = function() {
+        localStorage.gemKey = prompt(`please provide your API key`)
+    }
+}, 1000);
+
 // application
-(async function(){
+(async function() {
     console.log(`async runtime\n${Date()}`);
     GEM = (await import(`${location.href}gem.mjs`)).GEM
-    // look for key
-    
 })();
