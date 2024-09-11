c3bf18f83ac7f3c355f363d38bd6aec884a69828
Jonas Almeida <jalmeida@mathbiol.org>
2024-09-11 05:58:06 -0400

    cleanup

--- gem.mjs
+++ gem.mjs
@@ -1,1 +1,12 @@
 console.log(`gem.mjs (gemini SDK) imported\n${Date()}`);
+
+class GEM {
+    constructor(key){
+        this.loadedAt=Date()
+        this.key = key
+    }
+}
+
+export{
+    GEM
+}
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 console.log(`index.js loaded\n${Date()}`);
 
+// module
+GEM = (await import('http://localhost:8000/gemini/gem.mjs')).GEM
+
+// application
 (async function(){
-    console.log(`async runtime\n${Date()}`)
+    console.log(`async runtime\n${Date()}`);
+    
     
 })()
