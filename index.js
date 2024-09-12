c464be2e75ec7c9db05aaab97f81f97cedf4acac
Jonas Almeida <jalmeida@mathbiol.org>
2024-09-12 11:22:06 -0400

    conversation mode

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ console.log(`index.js loaded\n${Date()}`);
 
 // application
 (async function() {
+    let conversation=[]  // system prompt?
     console.log(`async runtime\n${Date()}`);
     const shdown = new ((await import('https://esm.sh/showdown@2.1.0')).default).Converter
     GEM = (await import(`${location.href}gem.mjs`)).GEM
@@ -10,18 +11,22 @@ console.log(`index.js loaded\n${Date()}`);
         localStorage.gemKey = prompt(`please provide your API key`)
     }
     promptTextArea.onkeydown = async function(ev) {
-        promptTextArea.focus()
+        //promptTextArea.focus()
         if((ev.key=='Enter')&(!ev.shiftKey)){
             //console.log(`Enter at ${Date()}`,ev)
             let div = document.createElement('div')
             responseDiv.appendChild(div)
+            conversation.push(promptTextArea.value)
             div.innerHTML=`<span style="color:DarkGreen">${promptTextArea.value}</span>`
-            let res = await g1.post(promptTextArea.value);
+            //let res = await g1.post(promptTextArea.value);
+            let res = await g1.post(conversation.join(' ; '));
             promptTextArea.value='...'
             div.innerHTML+=`<p style="color:blue">${shdown.makeHtml(res.candidates[0].content.parts[0].text)}</p><hr>`
+            conversation.push(res.candidates[0].content.parts[0].text)
             promptTextArea.value=''
             promptTextArea.focus()
-            console.log(res.candidates,Date())
+            console.log(res.candidates,conversation)
+            
         }
     }
 })();
