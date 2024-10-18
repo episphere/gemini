6a6a73b81d0d308c21c6410100a12d6b96232ac5
Jonas Almeida <jalmeida@mathbiol.org>
2024-10-18 16:53:20 -0400

    validate key and modularize chat

--- chat.mjs
+++ chat.mjs
@@ -1,2 +1,42 @@
-console.log(`stand alone gemini chat being tested at \n${Date()}`)
+console.log(`stand alone gemini chat deployed at \n${Date()}`)
+
+const mod = (await  import('./gem.mjs'))
+const gem = new mod.GEM(mod.validKey())
+const post = gem.post
+post('hello')  // testing
+
+
+//export{
+//    post
+//}
+
+//get post from instance of GEM
+//post = (new (await  import('./gem.mjs')).GEM).post
+
+/*
+        this.chat=async function(div,url='https://episphere.github.io/gemini/connectStudy.txt'){ // target div and context url 
+            console.log(`chatting ...`)
+            let txt = await (await fetch(url)).text()
+            if(typeof(div)=='string'){
+                div = document.getElementById(div)
+            }
+            if(!div){
+                div = document.createElement('div')
+                document.body.appendChild(div)
+            }
+            // scafold 
+            div.innerHTML=`<div id="divQuestion"></div><hr><textarea id="txtPrompt">experimental bot, don't take seriously</textarea>`
+            // operate on textarea
+            let txtPrompt = div.querySelector('#txtPrompt')
+            let that=this
+            txtPrompt.onkeyup=async function(ev){
+                if(ev.key=='Enter'){
+                    console.log(ev.target.value,that)
+                    let ans = await that.post(ev.target.value)
+                    console.log(ans)
+                }
+            }
+            4
+        }
+        */
 
